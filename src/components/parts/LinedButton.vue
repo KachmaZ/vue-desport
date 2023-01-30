9003465ac8f4a4b65477c39e3218bb5e50260ed4
Arthur Kachmazov <boss.kachmaz@mail.ru>
2023-01-30 22:19:59 +0300

    LinedButton component

---
+++ src/components/parts/LinedButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="lined-btn text-2">
+    <router-link :to="to">
+      <slot><!-- optional fallback --></slot>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    to: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.lined-btn {
+  width: 170px;
+  //   margin-left: 25px;
+
+  //   text-align: left;
+
+  border-bottom: 1px solid #000;
+}
+</style>
