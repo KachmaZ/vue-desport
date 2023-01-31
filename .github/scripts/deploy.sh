b3cb8a364cecedb99e7563293b6fd8a8e1a43e0a
Arthur Kachmazov <boss.kachmaz@mail.ru>
2023-01-31 21:04:30 +0300

    Deploy script update

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -7,17 +7,14 @@ set -e
 npm i --force
 npm run build
 
-# переход в каталог сборки
-cd dist
+# # переход в каталог сборки
+# cd dist
 
 # если вы публикуете на пользовательский домен
 
-git init
-git config --global user.email "boss.kachmaz@mail.ru"
-git config --global user.name "KachmaZ"
-git add -A
-git commit -m 'deploy'
+# git config --global user.email "boss.kachmaz@mail.ru"
+# git config --global user.name "KachmaZ"
+git add dist
+git commit -m 'Deploy'
 
-git push -f git@github.com:kachmaz/desport.git master:gh-pages
-
-cd -
+git subtree push --prefix dist origin gh-pages
